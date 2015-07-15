bd4f80a391a0478d0a86638d8d960262084fe593
lambdalisue <lambdalisue@hashnote.net>
2015-07-15 21:49:27 +0900

    Fix current working directory of PyMOL

--- PyMOL.app/Contents/MacOS/PyMOL.sh
+++ PyMOL.app/Contents/MacOS/PyMOL.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 # Start PyMOL with a working directoty which the specified file exists
-if [[ -f "$1" ]]; then
-    cd "$(dirname \"$1\")"
+if [ -f "$1" ]; then
+    cd $(dirname $1)
 fi
 /usr/local/bin/pymol $@
